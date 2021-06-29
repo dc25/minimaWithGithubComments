41b2a6f825328baae548d09529e1d40eac7d0d05
Dave Compton <davecompton7@gmail.com>
2021-06-29 18:30:53 +0000

    no longer using jquery.   Now using async/await/fetch

--- js/github-comments.js
+++ js/github-comments.js
@@ -1,62 +1,67 @@
 // Original code taken with permission from : https://github.com/dwilliamson/donw.io/blob/master/public/js/github-comments.js
 
-// use of ajax vs getJSON for headers use to get markdown (body vs body_html)
-
-function ShowComments(repo_name, comment_id, page_id)
+async function getComments(repo_name, comment_id, page_id, acc )
 {
-    $.ajax({
-        url: "https://api.github.com/repos/" + repo_name + "/issues/" + comment_id + "/comments" + "?page=" + page_id,
-        headers: {Accept: "application/vnd.github.v3.html+json"},
-        dataType: "json",
-        success: function(comments, textStatus, jqXHR) {
+    const url = "https://api.github.com/repos/" + repo_name + "/issues/" + comment_id + "/comments" + "?page=" + page_id;
+    const resp = await fetch (url, { headers: {Accept: "application/vnd.github.v3.html+json"}});
+    if (resp.status !== 200) {
+        throw new Error("Response status " + resp.status + " while attempting to fetch comments from: " + url);
+    }
 
-            if (1 == page_id) {
-                // post button 
-                var url = "https://github.com/" + repo_name + "/issues/" + comment_id + "#new_comment_field";
-                $("#gh-comments-list").append("<form action='" + url + "' rel='nofollow'> <input type='submit' value='Post a comment on Github' /> </form>");
+    const comments = await resp.json();
+    
+    if (1 == page_id) {
+        // post button 
+        const url = "https://github.com/" + repo_name + "/issues/" + comment_id + "#new_comment_field";
+        acc += ("<form action='" + url + "' rel='nofollow'> <input type='submit' value='Post a comment on Github' /> </form>");
+    }
+
+    // Individual comments
+    for (let i = 0; i < comments.length; i++)
+    {
+        const comment = comments[i];
+        const date = new Date(comment.created_at);
+
+        acc += "<div id='gh-comment'>";
+        acc += "<img src='" + comment.user.avatar_url + "' width='24px'>";
+        acc += "<b><a href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>";
+        acc += " posted at ";
+        acc += "<em>" + date.toUTCString() + "</em>";
+        acc += "<div id='gh-comment-hr'></div>";
+        acc += comment.body_html;
+        acc += "</div>";
+    }
+
+    // Call recursively if there are more pages to display
+
+    const links = resp.headers.get("Link");
+    if (links) {
+        const entries = links.split(",");
+        for (let j=0; j<entries.length; j++)
+        {
+            const entry = entries[j];
+            if ("next" == entry.match(/rel="([^"]*)/)[1])
+            {
+                acc = await getComments(repo_name, comment_id, page_id+1, acc);
+                break;
             }
-
-            // Individual comments
-            $.each(comments, function(i, comment) {
-
-                var date = new Date(comment.created_at);
-
-                var t = "<div id='gh-comment'>";
-                t += "<img src='" + comment.user.avatar_url + "' width='24px'>";
-                t += "<b><a href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>";
-                t += " posted at ";
-                t += "<em>" + date.toUTCString() + "</em>";
-                t += "<div id='gh-comment-hr'></div>";
-                t += comment.body_html;
-                t += "</div>";
-                $("#gh-comments-list").append(t);
-            });
-
-            // Call recursively if there are more pages to display
-            var linksResponse = jqXHR.getResponseHeader("Link");
-            if (linksResponse) {
-                var entries = linksResponse.split(",");
-                for (var j=0; j<entries.length; j++)
-                {
-                    var entry = entries[j];
-                    if ("next" == entry.match(/rel="([^"]*)/)[1])
-                    {
-                        ShowComments(repo_name, comment_id, page_id+1);
-                        break;
-                    }
-                }
-            }
-        },
-        error: function() {
-            $("#gh-comments-list").append("Comments are not open for this post yet.");
         }
-    });
+    }
+    return acc;
 }
 
 function DoGithubComments(repo_name, comment_id)
 {
-    $(document).ready(function ()
+    document.addEventListener("DOMContentLoaded", async function() 
     {
-        ShowComments(repo_name, comment_id, 1);
+        try {
+            const comments = await getComments(repo_name, comment_id, 1, "");
+            const commentsElement = document.getElementById('gh-comments-list');
+            commentsElement.innerHTML = comments;
+        } catch (err)
+        {
+            console.log( err.message );
+        }
+        return null;
     });
 }
